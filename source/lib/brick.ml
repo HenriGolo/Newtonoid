2ecc84eb94098cf945c84271c61dfbae1cacd92e
L6uis9 <louisdavid.duman@gmail.com>
2025-12-24 13:29:31 +0100

    add draw fct to brick

--- source/lib/brick.ml
+++ source/lib/brick.ml
@@ -6,3 +6,7 @@ type t = {
 }
 
 let create ~x ~y ~width ~height = { x; y; width; height }
+
+let draw (b : t) =
+  let open Graphics in
+  draw_rect (int_of_float b.x) (int_of_float b.y) (int_of_float b.width) (int_of_float b.height)
--- source/lib/brick.mli
+++ source/lib/brick.mli
@@ -7,3 +7,6 @@ type t = {
 
 (** Creation of a brick **)
 val create : x:float -> y:float -> width:float -> height:float -> t
+
+(** Draw the brick on the screen **)
+val draw : t -> unit
