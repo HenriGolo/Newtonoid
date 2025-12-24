a10dcca91c27c9eea411896429d326ae720f7bf4
L6uis9 <louisdavid.duman@gmail.com>
2025-12-24 13:31:15 +0100

    update ball with bounce functions, move, and drawing functions

--- source/lib/ball.ml
+++ source/lib/ball.ml
@@ -7,3 +7,18 @@ type t = {
 }
 
 let create ~x ~y ~radius ~vx ~vy = { x; y; radius; vx; vy }
+
+let move ball dt =
+  {
+    ball with
+    x = ball.x +. ball.vx *. dt;
+    y = ball.y +. ball.vy *. dt;
+  }
+
+let bounce_x ball = { ball with vx = -. ball.vx }
+let bounce_y ball = { ball with vy = -. ball.vy }
+let bounce_corner ball = { ball with vx = -. ball.vy; vy = -. ball.vx }
+
+let draw (b : t) =
+  let open Graphics in
+  fill_circle (int_of_float b.x) (int_of_float b.y) (int_of_float b.radius)
--- source/lib/ball.mli
+++ source/lib/ball.mli
@@ -8,3 +8,14 @@ type t = {
 
 (** Creation of a ball **)
 val create : x:float -> y:float -> radius:float -> vx:float -> vy:float -> t
+
+(** Move the ball according to its velocity and the time delta **)
+val move : t -> float -> t
+
+(* Bouncing functions *)
+val bounce_x : t -> t
+val bounce_y : t -> t
+val bounce_corner : t -> t
+
+(** Draw the ball on the screen **)
+val draw : t -> unit
