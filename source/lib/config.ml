4fe2cd29d6a807dd386044c46028e1b7eddc68f3
L6uis9 <louisdavid.duman@gmail.com>
2025-12-24 13:31:38 +0100

    adding config file

---
+++ source/lib/config.ml
@@ -0,0 +1,5 @@
+let screen_w = 800.
+let screen_h = 600.
+let g = -9.81
+let ball = Ball.create ~x:400. ~y:120. ~radius:8. ~vx:200. ~vy:200.
+let paddle = Paddle.create ~x:350. ~y:80. ~width:100. ~height:15.
