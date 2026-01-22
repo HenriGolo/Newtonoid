52346fa440757cdf156824b3d8d1c26e1c4d97a7
Crosnier Jeremie <jcrosnie@gorgone.enseeiht.fr>
2026-01-22 17:17:51 +0100

    adding of a part of the contrats

--- source/lib/ball.ml
+++ source/lib/ball.ml
@@ -6,8 +6,25 @@ type t = {
   vy : float;
 }
 
+(* CONTRACT
+   Function that creates a new ball instance.
+   Type: x:float -> y:float -> radius:float -> vx:float -> vy:float -> t
+   Parameter: x (float), the horizontal coordinate of the bottom-left corner
+   Parameter: y (float), the vertical coordinate of the bottom-left corner
+   Parameter: radius (float), the the radius of the ball
+   Parameter: vx (float), the horizontal speed of the ball
+   Parameter: vy (float), the vertical speed of the ball
+   Result: an instance of type t initialized with the given coordonates, radius and velocity.
+*)
 let create ~x ~y ~radius ~vx ~vy = { x; y; radius; vx; vy }
 
+(* CONTRACT
+   Function that simulates the movement of a ball during a dt
+   Type: t -> float -> t
+   Parameter: ball (t), the ball we want to move
+   Parameter: dt (float), the vertical coordinate of the bottom-left corner
+   Result: an instance of type t initialized with the given coordonates, radius and velocity.
+*)
 let move ball dt =
   {
     ball with
@@ -19,6 +36,12 @@ let bounce_x ball = { ball with vx = -. ball.vx }
 let bounce_y ball = { ball with vy = -. ball.vy }
 let bounce_corner ball = { ball with vx = -. ball.vy; vy = -. ball.vx }
 
+(* CONTRACT
+   Function that renders the ball on the screen.
+   Type: t -> unit
+   Parameter: ball (t), the ball instance
+   Result: (unit), draws a filled circle representing the ball.
+*)
 let draw (b : t) =
   let open Graphics in
   fill_circle (int_of_float b.x) (int_of_float b.y) (int_of_float b.radius)
--- source/lib/collision.ml
+++ source/lib/collision.ml
@@ -1,4 +1,12 @@
-(* Return couple new ball state + if it collides with the brick*)
+(* CONTRAT
+   Function that manages collision detection and bounce between a ball and a brick.
+   Type : Ball.t -> Brick.t -> (Ball.t * bool)
+   Parameter : ball (Ball.t), current state of the ball.
+   Parameter : brick (Brick.t), brick for which we want to check if there is a collision with the ball.
+   Return : A couple (new_ball, collision_detected) where :
+              - new_ball is the ball with a modified speed in case of a bounce, or the original ball otherwise.
+              - collision_detected is a Boolean value that is true if contact has occurred.
+*)
 let collision (ball:Ball.t) (brick:Brick.t) =
   (* we suppose 0,0 is bottom left *)
 
